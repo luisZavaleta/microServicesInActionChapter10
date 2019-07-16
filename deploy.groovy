da59c79e9ba982af483b7a16443d91115cdc7c0d
Luis Zavaleta <luiszavaleta@Luiss-MacBook-Air.local>
2019-07-16 13:32:37 -0500

    Formating

--- deploy.groovy
+++ deploy.groovy
@@ -1,16 +1,10 @@
 def toKubernetes(tagToDeploy, namespace, deploymentName) {
   sh("sed -i.bak 's#BUILD_TAG#${tagToDeploy}#' ./deploy/${namespace}/*.yml")
-
-
-
-
- container('kubectl') {
+  
+  container('kubectl') {
     sh("kubectl --namespace=${namespace} apply -f deploy/${namespace}/")
   }
-  
 }
-
-
 
 def rollback(deploymentName) {
   kubectl("rollout undo deployment/${deploymentName}")
